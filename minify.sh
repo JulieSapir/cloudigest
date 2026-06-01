4a56233ea7027f8b3b9770987bf403539510d4e2
ALisaKhata <mirco_galvez@usmp.pe>
2026-06-01 23:35:36 +0800

    feat: update minification script to process all JavaScript files and remove unused minified file

--- minify.sh
+++ minify.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-
-npx terser snippets.js -o snippets.min.js --compress --mangle --toplevel
+shopt -s nullglob
+for file in *.js; do
+    if [[ "$file" != *.min.js ]]; then
+        echo "Minifying $file..."
+        npx terser "$file" -o "build/${file%.js}.min.js" --compress --mangle --toplevel
+    fi
+done
